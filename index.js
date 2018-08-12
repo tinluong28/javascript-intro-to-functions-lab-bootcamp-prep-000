4e6a637804b0f24a82b7d57864bcd44f5d61ba7c
tinluong28@aol.com <tinluong28@aol.com>
2018-08-12 02:00:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma (string){
     if(string.toLowerCase() === string){
 	return `I can\'t hear you.`}
 	else if(string.toUpperCase() === string){
-	console.log('YES INDEED!')}
+	return `YES INDEED!`}
 	else if(string === "I love you, Grandma."){
 	console.log('I love you, too!')}
 }
