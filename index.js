28b0d6e2c71d8fa5b8ab8ca7ff61b216d11f0857
tinluong28@aol.com <tinluong28@aol.com>
2018-08-12 01:56:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,9 +20,9 @@ function logWhisper(string)
 
 function sayHiToGrandma (string){
     if(string.toLowerCase() === string){
-	console.log('I can\'t hear you.')}
+	console.log('I can\'t hear you!')}
 	else if(string.toUpperCase() === string){
 	console.log('YES INDEED!')}
-	else if(string === "I love you"){
+	else if(string === "I love you, Grandma."){
 	console.log('I love you, too!')}
 }
